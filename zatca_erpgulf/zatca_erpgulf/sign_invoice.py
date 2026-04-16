7a165096faccd021643e78ac21189e2131e81857
hamzaali15 <malikhamzaali48@gmail.com>
2026-04-16 18:22:00 +0500

    updated

--- zatca_erpgulf/zatca_erpgulf/sign_invoice.py
+++ zatca_erpgulf/zatca_erpgulf/sign_invoice.py
@@ -14,6 +14,7 @@ import base64
 import json
 from frappe import _
 import frappe
+from erpnext import get_region
 import requests
 from zatca_erpgulf.zatca_erpgulf.event_log import log_zatca_event
 from pyqrcode import create as qr_create
