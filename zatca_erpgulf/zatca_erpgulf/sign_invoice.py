5d110d72e6279558669a34c8f5c8e17432b264c6
hamzaali15 <malikhamzaali48@gmail.com>
2026-04-16 18:34:54 +0500

    updated

--- zatca_erpgulf/zatca_erpgulf/sign_invoice.py
+++ zatca_erpgulf/zatca_erpgulf/sign_invoice.py
@@ -15,6 +15,9 @@ import json
 from frappe import _
 from erpnext import get_region
 import requests
+import xmltodict
+import re
+import html
 from zatca_erpgulf.zatca_erpgulf.event_log import log_zatca_event
 from pyqrcode import create as qr_create
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
